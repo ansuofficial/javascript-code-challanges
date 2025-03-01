a1696c9bbe3fe168655e92f5be0d1b1be5aca284
Ansuofficial <badjieansu165@gmail.com>
2025-03-01 14:43:36 +0000

    A function that returns the sum of two lowest values

--- 0-return-the-sum-of-the-two-lowest-numbers.js
+++ 0-return-the-sum-of-the-two-lowest-numbers.js
@@ -1,6 +1,6 @@
 /*
 
-0. A function that returns the sum of the two lowest positive numbers.
+0. A function that returns the sum of the two lowest numbers.
 
 Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers.
 
