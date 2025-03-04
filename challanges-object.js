950f849f2696be63b378803d31def8ae3e1b26d2
Ansuofficial <badjieansu165@gmail.com>
2025-03-04 15:33:24 +0000

    updating challenge object

--- challanges-object.js
+++ challanges-object.js
@@ -1,13 +1,25 @@
 // javascript-code-challanges
-
-const challanges = [
+const challenges = [
   {
     "code-challange": 0,
     "challange-description":
-      "Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers.",
+      "Create a function that returns the sum of the two lowest positive numbers given an array of positive integers.",
     difficulty: "Easy",
     resources:
       "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array",
-    notes: "this challange involve working with arrays",
+    notes: "This challenge involves working with arrays.",
+  },
+  {
+    "code-challange": 1,
+    "challange-description":
+      "Write a function that takes numbers as a parameter and returns the average of the numbers.",
+    difficulty: "Easy",
+    resources: [
+      "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce",
+      "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach",
+      "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for",
+    ],
+    notes:
+      "This challenge involves iterating over an array and calculating the average.",
   },
 ];
